1a7c4772d3c583035c1a78244d4208e1374ce3ef
EK <erickopcha@gmail.com>
2022-07-18 22:50:50 +0300

    feat: header UI

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,42 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import SearchIcon from '@mui/icons-material/Search';
+import AirplaneTicketIcon from '@mui/icons-material/AirplaneTicket';
+import {
+  Search,
+  SearchIconWrapper,
+  StyledInputBase,
+  TypographyCentered
+} from './Header.styled';
+
 const Header = () => {
   return (
-    <>Header</>
-  )
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <TypographyCentered
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'flex' } }}
+          >
+            <AirplaneTicketIcon style={{ marginRight: '5px' }} />
+            Travel Advisor
+          </TypographyCentered>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
 
 export default Header;
