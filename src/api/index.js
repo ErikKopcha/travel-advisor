8e3be94da596b0fd5681e17f3650607d7fd7a265
EK <erickopcha@gmail.com>
2022-07-25 22:13:00 +0300

    fix: api, feat: default data

--- src/api/index.js
+++ src/api/index.js
@@ -4,7 +4,8 @@ const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
 
 const getPlacesData = async ({ sw, ne }) => {
   try {
-    return null;
+    if (!sw || !ne) return [];
+    return [];
 
     const { data: { data } } = await axios.get(URL, {
       params: {
@@ -14,7 +15,7 @@ const getPlacesData = async ({ sw, ne }) => {
         tr_longitude: ne.lng,
       },
       headers: {
-        'X-RapidAPI-Key': 'c1e913e40cmshb43bbd6ec50979ap1b0706jsn42868e8c7a83',
+        'X-RapidAPI-Key': '',
         'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
       }
     });
@@ -22,9 +23,15 @@ const getPlacesData = async ({ sw, ne }) => {
     return data;
   } catch (e) {
     console.error(`getPlacesData error: `, e);
+    return [];
   }
 }
 
+const getDefaultPlacesData = async () => {
+  return await (await fetch(`http://localhost:3001/data`)).json();
+}
+
 export {
-  getPlacesData
+  getPlacesData,
+  getDefaultPlacesData
 };
